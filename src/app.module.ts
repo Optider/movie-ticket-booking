bfa6605d15ffcd5941f6cae7c896cd5451268dd2
Anuj Sharma <anujsharmasppu@gmail.com>
2022-06-19 21:12:29 +0530

    Update logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,6 +32,7 @@ import { APP_PIPE } from '@nestjs/core';
       transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
         new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console({ level: 'debug' }),
       ],
     }),
   ],
